203ac3fd026cabe426e271e49b7f378ff0e6cb0d
brayanalvarez98 <71362126+brayanalvarez98@users.noreply.github.com>
2021-09-29 08:50:41 -0500

    Add files via upload

---
+++ src/main/java/com/G46Tienda/Tienda/G46TiendaApplication.java
@@ -0,0 +1,13 @@
+package com.G46Tienda.Tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G46TiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G46TiendaApplication.class, args);
+	}
+
+}
